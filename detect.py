9c60e73dfdb2542f2cbcd67443d8878c76672913
cucumber6621601123 <phiriyaporn.y@ku.th>
2025-03-14 23:45:10 +0700

    train5 best3

--- detect.py
+++ detect.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("best_1.pt")
+model = YOLO("best_3.pt")
 
-results = model("test/istockphoto-505755464-612x612.jpg")
+results = model("test/istockphoto-1080293344-612x612.jpg")
 results[0].show()
