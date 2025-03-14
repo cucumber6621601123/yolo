f7a2af321c677d75964fb15811ebc6879dfa6597
cucumber6621601123 <phiriyaporn.y@ku.th>
2025-03-15 00:16:30 +0700

    ,

--- detect.py
+++ detect.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO("best_3.pt")
 
-results = model("test/istockphoto-1080293344-612x612.jpg")
+results = model("test/istockphoto-1371845520-612x612.jpg")
 results[0].show()
